7aa5d97c685a2556f13e59de2b0aee89898023c6
Btara Truhandarien <btara.truhandarien@gmail.com>
2019-02-22 16:35:59 -0500

    Modify scraper to be more flexible and ability to handle more sections

--- indeed-scraper.py
+++ indeed-scraper.py
@@ -4,169 +4,210 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 import time
 import json
 import threading
 from random import randint
 from time import sleep
+from math import ceil
 import os
+import argparse
+from urllib.parse import quote_plus
+import traceback
+import sys
+
+NUM_INDEED_RESUME_RESULTS = 50
+INDEED_RESUME_BASE_URL = 'https://resumes.indeed.com/resume/'
+INDEED_RESUME_SEARCH_BASE_URL = 'https://resumes.indeed.com/search/'
+
+# RESUME SUBSECTIONS TITLE (in normal setting)
+WORK_EXPERIENCE = 'Work Experience'
+EDUCATION = 'Education'
+SKILLS = 'Skills'
+CERTIFICATIONS = 'Certifications'
+ADDITIONAL_INFORMATION = 'Additional Information'
 
 class Resume(object):
-
-	def __init__ (self, idd, jobs, schools):
+	def __init__ (self, idd, jobs=None, schools=None):
 		self.id = idd
 		self.jobs = jobs
 		self.schools = schools
 
 	def toJSON(self):
 		return json.dumps(self, default=lambda o: o.__dict__, 
-			sort_keys=True, indent=0)
+			sort_keys=True, indent=2)
 
 
 class Job(object):
-	def __init__(self, title, company, location, hire_date):
+	def __init__(self, title, company, work_dates, details):
 		self.title = title
 		self.company = company
-		self.location = location
-		self.hire_date = hire_date
+
+		dates = work_dates.split(' to ')
+		self.start_date = dates[0]
+		self.end_date = '' if len(dates) == 1 else dates[1]
+
+		self.details = details
 
 class School(object):
 	def __init__(self, degree, school_name, grad_date):
 		self.degree = degree
 		self.school_name = school_name
-		self.grad_date = grad_date
+		self.start_date = ''
+		self.end_date = ''
 
+		if grad_date is not None:
+			dates = grad_date.split(' to ')
+			self.start_date = dates[0]
+			self.end_date = '' if len(dates) == 1 else dates[1]
 
 def gen_idds(url, driver):
 
 	driver.get(url)
-	p_element = driver.page_source
-	soup = BeautifulSoup(p_element, 'html.parser')
-	links = soup.find_all(class_="icl-TextLink icl-TextLink--primary rezemp-u-h4")
 
-	idds=[]
-
-	for link in links:
-		path = link.get("href")
-		#print(path[8:path.find("?")]) #8 is to account for "\resume\" at the beginning
-		idds.append(path[8:path.find("?")])
+	resume_links = []
+	try:
+		resume_links = driver.find_elements_by_css_selector('.icl-TextLink.icl-TextLink--primary.rezemp-u-h4')
+	except TimeoutException:
+		# could not complete in time 
+		resume_links = []
+	finally:
+		idds = [link.get_attribute('href') for link in resume_links]
+		idds = [idd[idd.rfind('/')+1:idd.rfind('?')] for idd in idds]
 
 	return idds
-#print(idds)
+
+def produce_work_experience(worksection):
+	work_experience = worksection.find_all('div', class_='rezemp-WorkExperience')
+	jobs = []
+	for experience in work_experience:
+		job_title = experience.find('div', class_='rezemp-u-h4').get_text()
+		company_and_dates = experience.find('div', class_='rezemp-WorkExperience-subtitle')
+
+		company_name = company_and_dates.find('span', class_='icl-u-textBold').get_text()
+		work_dates = company_and_dates.find('div', class_='icl-u-textColor--tertiary').get_text()
+		job_details = []
+		if len(experience.contents) == 3:
+			# there are job details
+			details = experience.contents[-1]
+			job_details = [detail for detail in details.stripped_strings]
+
+		jobs.append(Job(job_title, company_name, work_dates, job_details))
+	return jobs
+
+def produce_education(edusection):
+	content = edusection.find('div', class_='rezemp-ResumeDisplaySection-content')
+	schools = []
+	for school in content.children:
+		degree = school.find(class_ = "rezemp-ResumeDisplay-itemTitle")
+		if degree is not None:
+			degree = degree.get_text(' ', strip=True)
+		university_details = school.find(class_="rezemp-ResumeDisplay-university")
+		school_name = university_details.find('div', class_='icl-u-textBold')
+		if school_name is not None:
+			school_name = school_name.get_text()
+		date = school.find(class_="rezemp-ResumeDisplay-date")
+		if date is not None:
+			date = date.get_text()
+		schools.append(School(degree, school_name, date))
+	return schools
+
+def produce_skills():
+	pass
+
+def produce_certifications_license():
+	pass
+
+def produce_info():
+	pass
 
 def gen_resume(idd, driver):
-	URL = '''https://resumes.indeed.com/resume/''' + idd
+	resume_url = INDEED_RESUME_BASE_URL + idd
 
-
-	driver.get(URL)
+	driver.get(resume_url)
 	p_element = driver.page_source
 	soup = BeautifulSoup(p_element, 'html.parser')
 
-	results = soup.find_all('div', attrs={"class":"rezemp-ResumeDisplaySection"})
-	#print(results)
+	resume_subsections = soup.find_all('div', attrs={"class":"rezemp-ResumeDisplaySection"})
 
+	resume_details = {
+		'jobs': None,
+		'schools': None
+	}
+	for subsection in resume_subsections:
+		children = subsection.contents
+		subsection_title = children[0].get_text()
+		if subsection_title == WORK_EXPERIENCE:
+			resume_details['jobs'] = produce_work_experience(subsection)
+		elif subsection_title == EDUCATION:
+			resume_details['schools'] = produce_education(subsection)
+		elif subsection_title == SKILLS:
+			produce_skills()
+		elif subsection_title == CERTIFICATIONS:
+			produce_certifications_license()
+		elif subsection_title == ADDITIONAL_INFORMATION:
+			produce_info()
+		else:
+			print('ID', idd, '- Subsection title is', subsection_title)
 
+	return Resume(idd, **resume_details)
 
-	schools = []
-
-
-	try:
-		education = results[1]
-		content = education.find(class_="rezemp-ResumeDisplaySection-content")
-		for uni in content.children:
-			degree = uni.find(class_ = "rezemp-ResumeDisplay-itemTitle").get_text()
-			university = uni.find(class_="rezemp-ResumeDisplay-university").contents[0].get_text()
-			date = uni.find(class_="rezemp-ResumeDisplay-date").get_text()
-			schools.append(School(degree, university, date))
-	except:
-		pass
-
-	jobs = []
-
-
-	try:
-		work_experience = results[0]
-		job_titles = work_experience.find_all(class_="rezemp-u-h4")
-		job_descriptions = work_experience.find_all(class_="rezemp-u-h5")
-		for i in range(len(job_titles)):
-			dates = job_descriptions[i].find_next_sibling().get_text()
-			date = dates[:dates.find("to")]
-			title = job_titles[i].get_text()
-			desc = [p.get_text() for p in job_descriptions[i].find_all("span")][1:]
-			company = desc[0]
-			location = desc[1]
-			jobs.append(Job(title, company, location, date))
-	except:
-		pass
-
-	return Resume(idd, jobs, schools)
-
-
-
-def mine(name, URL, override=True, rangee=None):
-	driver = webdriver.Chrome()
+def mine(filename, URL, override=True, search_range=None):
+	driver = webdriver.Firefox()
 	driver.implicitly_wait(10)
 
+	if override:
+		# implicitly empty file
+		open(filename, 'w').close()
 
-	if(override):
-		with open('resume_output' + name + '.json', 'w') as outfile:
-			outfile.write("")
+	search = search_range[0]
+	end = search_range[1]
 
+	try:
+		while search < end:
+			stri = URL + '&start=' + str(search)
+			idds = gen_idds(stri, driver)
+			# move to the next results irrespective
+			search += NUM_INDEED_RESUME_RESULTS
+			if(len(idds) == 0):
+				time.sleep(2)
+				continue
 
-	if rangee == None:	
-		start_index = 700
-		target = 10901
-	else:
-		start_index = rangee[0]
-		target = rangee[1]
+			with open(filename, 'a') as outfile:
+				for idd in idds:
+					outfile.write(gen_resume(idd, driver).toJSON() + "\n")
 
-
-	print(start_index, target)
-
-	while 1:
-		if (start_index >= target):
-			break
-		
-		stri = URL+"&start="+str(start_index)
-		print(stri)
-		# wait = WebDriverWait(driver, 20)
-		# element = wait.until(EC.presence_of_all_elements_located())
-		idds = gen_idds(URL+"&start="+str(start_index), driver)
-		print(idds)
-		if(len(idds) == 0):
-			time.sleep(4)
-			continue
+		# resumes = [gen_resume(idd).toJSON() for idd in idds] 
+	except Exception as e:
+		sys.stderr.write('Caught exception ' + str(e) + traceback.format_exc() + "\n")
+	finally:
+		driver.close()
 
 
-
-
-		for idd in idds:
-			with open('resume_output' + name + '.json', 'a') as outfile:
-				json.dump(gen_resume(idd, driver).toJSON(), outfile)
-
-		start_index+=50
-
-	# resumes = [gen_resume(idd).toJSON() for idd in idds] 
-
-	driver.close()
-
-
-def mine_multi(name, url, override=True):
-
-
+def mine_multi(args, search_URL):
 	thread_list = []
 	names = []
 
-	target = 8000
-	tr = 8
-	for i in range(tr):
+	start = args.si
+	end = args.ei
+	tr = args.threads
+	starting_points = list(range(start, end, (end - start) // tr))
+	for idx, search_start in enumerate(starting_points):
 		# Instantiates the thread
-		# (i) does not make a sequence, so (i,)
-		t = threading.Thread(target=mine, args=(name+str(i),url,), kwargs={"override" : override, "rangee" : (i*(target//tr), (i+1)*(target//tr)),})
+		filename = 'resume_output' + args.name + str(idx) + '.json'
+		t = threading.Thread(
+			target=mine,
+			args=(filename, search_URL),
+			kwargs={
+				"override" : args.override,
+				"search_range" : (search_start, end if idx + 1 == len(starting_points) else starting_points[idx + 1])
+			}
+		)
 		# Sticks the thread in a list so that it remains accessible
 		thread_list.append(t)
-		names.append("resume_output" + name + str(i) +".json")
+		names.append(filename)
 
 	# Starts threads
 	for thread in thread_list:
@@ -177,12 +218,11 @@ def mine_multi(name, url, override=True):
 	for thread in thread_list:
 		thread.join()
 
+	consolidate_files(args.name, names, args.override)
 
-	consolidate_files(name, names)
-
-
-def consolidate_files(name, names):
-	file = open("resume_output" + name + ".json", "a")
+def consolidate_files(name, names, override=True):
+	mode = 'w' if override else 'a'
+	file = open("resume_output_" + name + ".json", mode)
 	for nam in names:
 		with open(nam, 'r') as read:
 			file.write(read.read())
@@ -190,29 +230,24 @@ def consolidate_files(name, names):
 			
 	file.close()
 
+def main(args):
+	t = time.clock()
+	# restrict search only to job titles skills and field of study
+	search_URL= f"{INDEED_RESUME_SEARCH_BASE_URL}?q={args.q}&l={args.l}&searchFields=jt,skills,fos"
+	search_URL = quote_plus(search_URL, safe='/:?&=')
 
-
-def main():
-
-
-
-	t = time.clock();
-
-	#idds = ["f845ad88e3d17704", "1992c61c49a470e1"]
-
-	#URL = "https://resumes.indeed.com/search?l=california&q=software%20engineer&searchFields="
-
-	URL = "https://resumes.indeed.com/search?q=doctor&l=california&searchFields="
-
-	#mine("software_engineers_california", URL, override = False)
-
-
-	#consolidate_files("lawyer-california", ["resume_outputlawyer-california" + str(i) +".json" for i in range(8)])
-
-	mine_multi("doctor-california", URL)
-
-
+	mine_multi(args, search_URL)
 	print(time.clock() - t),
 
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description='Scrape Indeed Resumes', usage='python indeed-scraper.py <arguments>')
+	parser.add_argument('-q', metavar='query', required=True, help='search query to run on indeed e.g software engineer')
+	parser.add_argument('-l', default='Canada', metavar='location', help='location scope for search')
+	parser.add_argument('-si', default=0, type=int, metavar='start', help='starting index (multiples of 50)')
+	parser.add_argument('-ei', default=5000, type=int, metavar='end', help='ending index (multiples of 50)')
+	parser.add_argument('--threads', default=8, type=int, metavar='threads', help='# of threads to run')
+	parser.add_argument('--override', action='store_true', help='override existing result if any')
+	parser.add_argument('--name', metavar='name', required=True, help='name of search (used to save files)')
 
-main()
+	args = parser.parse_args()
+	main(args)
